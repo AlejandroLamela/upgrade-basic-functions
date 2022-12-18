2e7a77ad334c9cdadaed828cfd3556ef53f0f786
alejandrolamela <alejandro.lamela@bootcamp-upgrade.com>
2022-12-18 13:22:59 +0100

    Iteración #1: Buscar el máximo

---
+++ iteracion_1.js
@@ -0,0 +1,12 @@
+//! Iteración #1: Buscar el máximo
+// Completa la función que tomando dos números como argumento devuelva el más alto.
+
+function sum(numberOne , numberTwo) {
+  if (numberOne>numberTwo) {
+    console.log(numberOne);
+  } else if(numberTwo>numberOne){
+    console.log(numberTwo);
+  }
+}
+
+sum(4,5);
