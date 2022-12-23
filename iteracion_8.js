31066c5144cf620edcd118e34382c7f9139f9638
alejandrolamela <alejandro.lamela@bootcamp-upgrade.com>
2022-12-23 17:20:00 +0100

    **Iteration #8: Contador de repeticiones**

--- iteracion_8.js
+++ iteracion_8.js
@@ -2,7 +2,6 @@
 
 // Crea una función que nos devuelva el número de veces que se 
 // repite cada una de las palabras que lo conforma.  Puedes usar este array para probar tu función:
-//? Este tengo que volver a hacerlo
 
 const counterWords = [
     'code',
@@ -18,18 +17,18 @@ const counterWords = [
     'code'
   ];
 
-  const result={};
-  repeatCounter(counterWords);
-  function repeatCounter(arrayCounterDuplicates) {
-    for (let index = 0; index < arrayCounterDuplicates; index++) {
-        for (let j = 0; j < arrayCounterDuplicates; j++) {
-            if (arrayCounterDuplicates[i] in result) {
-                // Lo de arriba ignifica si arraycounter.. esta dentro del onjeto resultado
-            } else {
-                
-            }
-            
+
+  function repeatCounter(array) {
+    let counter = {};
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] in counter) {
+            counter[array[i]]++;
+        } else{
+            counter[array[i]]=1;
         }
-        
+      
     }
+     return console.log(counter); 
   }
+
+  repeatCounter(counterWords);
